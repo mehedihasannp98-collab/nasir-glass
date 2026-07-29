d2e21f40a208dfa2afe349873ddbcdec3ab51e9b
mehedihasannp98-collab <mehedihasan.np98@gmail.com>
2026-07-29 15:19:12 +0600

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = 'nasir-glass-v23';
+const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)).catch(()=>{})
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  // Network-first for everything so prices/parties/invoices stay fresh when
+  // online, but fall back to the cached shell when offline.
+  event.respondWith(
+    fetch(event.request)
+      .then((res) => {
+        const copy = res.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(event.request).then((r) => r || caches.match('./index.html')))
+  );
+});
