58e37739241654c2500e13cb6c2bf8fb593cfa2d
mehedihasannp98-collab <mehedihasan.np98@gmail.com>
2026-07-29 16:29:13 +0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'nasir-glass-v23';
+const CACHE_NAME = 'nasir-glass-v25';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
